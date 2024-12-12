6240c889be8c2f88f32830bbe5d798d387ee333e
Mike Nomitch <mnomitch@cloudflare.com>
2024-12-11 20:00:16 -0800

    convering to entrypoint

--- src/entrypointC.js
+++ src/entrypointC.js
@@ -1,16 +1,15 @@
-// This can be defined in the same codebase as the main Worker,
+// This can be defined in the same codebase as the main entrypoint,
 // but it doesn't share a top-level context with anybody unnecessarily
 
 // It can reuse bindings defined in the main wrangler.toml file
 
-export default {
-  bindingName: 'WORKER_C',
+export default class EntrypointC extends WorkerEntrypoint {
+  bindingName: 'ENTRYPOINT_C',
   placement: 'smart',
 
   async fetch(request, env, ctx) {
-    // using a binding also defined in the main wrangler.toml file
     await fetch(env.METRICS_URL, { method: 'PUT' });
 
-    return new Response('Hello from Worker C');
+    return new Response('Hello from Entrypoint C');
   },
 };
